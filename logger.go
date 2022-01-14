e1330561175ca2da9437a1ac6bbd1d651a97a155
Pattycakes <contact@patrickgroh.com>
2022-01-14 10:30:46 -0600

    Update logger.go
    Fleshing it out more, idk if it even compiles tho

--- logger.go
+++ logger.go
@@ -1,11 +1,15 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+)
 
-type LogType int
+type LogType uint8
 const (
-	Console LogType = 0
-	File            = 1
+	Console LogType = 1 << iota
+	File
+	
 )
 
 type LogSeverity int
@@ -16,15 +20,46 @@ const (
 	Critical          = 3
 )
 
+//RatLogger is a wrapper around the normal Go logger that just adds logging levels 
 type RatLogger struct {
-	loggerType LogType
-	filename   string
-	LogChannel chan
-	logLevel   LogSeverity
-	
+	loggerType  LogType
+	filename    string
+	LogChannel  chan LogPacket
+	logLevel    LogSeverity
+	logger      Logger
+	file        os.File
 }
 
 type LogPacket struct {
 	Severity LogSeverity
 	content  string
 }
+
+func NewLogger(logtype LogType, filename string, logLevel logSeverity) *RatLogger {
+	var l RatLogger
+	l.loggerType = logtype
+	l.filename = filename
+	l.logLevel = logLevel
+	
+	f, err := os.OpenFile(filename, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0644)
+	if err != nil {
+	    log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Println("This is a test log entry")
+}
+
+func (l *RatLogger) HandleLogs() {
+	
+}
+
+func (l *RatLogger) Close() {
+	defer l.file.Close()
+	close(l.LogChannel)
+}
+
+
+
+
